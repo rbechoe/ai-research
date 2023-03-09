29fb26c41b3f11123c336c4bed80ccc8963f65d5
Ravi Bechoe <35457924+rbechoe@users.noreply.github.com>
2023-03-09 18:01:46 +0100

    Update DebugLogNode.cs

--- AI research project/Assets/Scripts/Nodes/DebugLogNode.cs
+++ AI research project/Assets/Scripts/Nodes/DebugLogNode.cs
@@ -6,17 +6,17 @@ public class DebugLogNode : ActionNode
 
     protected override void OnStart()
     {
-        Debug.Log($"OnStart{message}");
+        //Debug.Log($"OnStart {message}");
     }
 
     protected override void OnStop()
     {
-        Debug.Log($"OnStop{message}");
+        //Debug.Log($"OnStop {message}");
     }
 
     protected override State OnUpdate()
     {
-        Debug.Log($"OnUpdate{message}");
+        Debug.Log($"OnUpdate {message}");
 
         //Debug.Log($"Blackboard:{blackboard.moveToPosition}");
         //blackboard.moveToPosition.x += 1;
